4d10401b54a6f7754984dcbfcc8876bff5eb0ef8
iceqiw <qqwei1123@163.com>
2017-07-11 14:02:33 +0800

    配置添加

--- src/main/java/org/qiwei/applicationServer.java
+++ src/main/java/org/qiwei/applicationServer.java
@@ -8,6 +8,7 @@ import org.qiwei.config.AppConfig;
 
 import javax.servlet.DispatcherType;
 import java.util.EnumSet;
+import java.util.ResourceBundle;
 
 /**
  * Created by admin on 2017/7/11.
@@ -15,19 +16,20 @@ import java.util.EnumSet;
 public class applicationServer {
 
     public static void main(String[] args) throws Exception {
+        ResourceBundle rb = ResourceBundle.getBundle("app");
         Server server = new Server();
         ServerConnector connector = new ServerConnector(server);
-        connector.setName("test1");
+        connector.setName(rb.getString("app.name"));
         connector.setHost("0.0.0.0");
-        connector.setPort(8080);
+        connector.setPort(Integer.parseInt(rb.getString("app.port")));
         server.addConnector(connector);
 
 
         WebAppContext webAppContext = new WebAppContext();
         webAppContext.setServer(server);
-        webAppContext.setResourceBase("/Tpl");
+        webAppContext.setResourceBase(rb.getString("app.resourceBase"));
         //访问地址：http://127.0.0.1:8080/test/
-        webAppContext.setContextPath("/test");
+        webAppContext.setContextPath(rb.getString("app.contextPath"));
         webAppContext.addFilter(GuiceFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
 
         webAppContext.addEventListener(new AppConfig());
